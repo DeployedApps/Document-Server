55015c95e4cedcf8ae6a862a5cb0efac36c48365
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 15:35:09 +0530

    Docs API Server

--- src/main/java/documentation/single/swagger/MsDocumentationApplication.java
+++ src/main/java/documentation/single/swagger/MsDocumentationApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -12,7 +16,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableConfigurationProperties({SwaggerServicesConfig.class,SwaggerServicesConfig.SwaggerServices.class})
 public class MsDocumentationApplication {
-
+//	public Docket apiDocket() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.any())
+//				.paths(PathSelectors.any())
+//				.build();
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(MsDocumentationApplication.class, args);
 	}
